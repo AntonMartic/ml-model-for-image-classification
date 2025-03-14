8ca089228ebf6219be3993a8f3d8500394950a37
Emil Djurson <emidj236@student.liu.se>
2025-03-14 18:16:23 +0100

    Lagt till shadcn och supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { useEffect, useState } from "react"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export default function page() {
   const [message, setMessage] = useState("Loading");
@@ -15,6 +17,12 @@ export default function page() {
     <>
       <div className="bg-gray-50 w-dvw h-dvh flex justify-center items-center flex-col gap-8">
         <h1 className="text-black font-extrabold text-7xl">Dog and cat classifier</h1>
+        <form className="w-8/12 h-1/4 bg-gray-200 rounded-3xl flex justify-center items-center">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="picture">Picture</Label>
+            <Input id="picture" type="file" accept="image/*" />
+          </div>
+        </form>
         <p className="text-black text-4xl font-bold">{message}</p>
       </div>
     </>
