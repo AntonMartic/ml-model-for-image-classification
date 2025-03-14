9aeb5f2abac895fe3ad1e822952e79dbdbd9fb2e
AntonMartic <anton.martic@gmail.com>
2025-03-14 13:06:42 +0100

    setup environment + test

---
+++ PythonProject1/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.feature import hog
+from skimage.color import rgb2gray
+from skimage.io import imread
+from skimage.transform import resize
+
+x = np.arange(0,1,0.01)
+print(x)
+
+# Create image directories
+cat_dir ="training-data/PetImages/Cat"
+cat_dog ="training-data/PetImages/Dog"
+
+# test resize and grayscale for a single image
+img_path = "training-data/PetImages/Cat/1.jpg"
+img = imread(img_path)  # Load the image
+img_resized = resize(img, (128, 128))  # Resize
+img_gray = rgb2gray(img_resized)  # Convert to grayscale
+
+plt.imshow(img_path)
+plt.imshow(img_resized)
+plt.imshow(img_gray)
