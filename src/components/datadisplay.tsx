e886d08483a12d6d5c43a30a5c5c8b571f1368b3
Emil Djurson <emil@djurson.se>
2025-03-24 18:10:18 +0100

    Frontend complete?

--- src/components/datadisplay.tsx
+++ src/components/datadisplay.tsx
@@ -3,25 +3,25 @@ import { DataDisplayProps } from "./types";
 export default function DataDisplay({ data }: DataDisplayProps) {
   return (
     <div className="grid grid-cols-2 gap-4">
-      <p className="text-left font-semibold text-neutral-950">Katter:</p>
-      <p className="text-right font-semibold text-neutral-950">
+      <p className="self-center pl-4 text-left font-semibold text-neutral-950">Katter:</p>
+      <p className="self-center pr-4 text-right font-semibold text-neutral-950">
         {data ? data.cat : 0}
       </p>
 
-      <p className="text-left font-semibold text-neutral-950">Hundar:</p>
-      <p className="text-right font-semibold text-neutral-950">
+      <p className="self-center pl-4 text-left font-semibold text-neutral-950">Hundar:</p>
+      <p className="self-center pr-4 text-right font-semibold text-neutral-950">
         {data ? data.dog : 0}
       </p>
 
-      <p className="text-left font-semibold text-neutral-950">
+      <p className="self-center pl-4 text-left font-semibold text-neutral-950">
         Korrekta klassifikationer:
       </p>
-      <p className="text-right font-semibold text-green-700">
+      <p className="self-center pr-4 text-right font-semibold text-green-700">
         {data ? data.correctClassifications : 0}
       </p>
 
-      <p className="text-left font-semibold">Felaktiga klassifikationer:</p>
-      <p className="text-right font-semibold text-red-700">
+      <p className="self-center pl-4 text-left font-semibold">Felaktiga klassifikationer:</p>
+      <p className="self-center pr-4 text-right font-semibold text-red-700">
         {data ? data.wrongClassifications : 0}
       </p>
     </div>
